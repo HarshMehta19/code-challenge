61bf21299a4ecbabb7337c1a15f1ec30dca8c0ad
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-04-18 00:31:19 -0300

    Time: 4 ms (99.58%) | Memory: 46.1 MB (8.66%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,9 +1,24 @@
 class Solution {
     int totalCount = 0;
     public int islandPerimeter(int[][] grid) {
-        int [][] visited = new int[grid.length][grid[0].length];
-        bfs(grid, 0, 0, visited);
-        return totalCount;
+        int perimeter = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for(int r =0;r<rows;r++) {
+            for(int c=0;c<cols;c++) {
+                if(grid[r][c] == 1) {
+                    perimeter += 4;
+                    if(r>0 && grid[r-1][c] == 1) perimeter -= 2;
+                    if(c>0 && grid[r][c-1] == 1) perimeter -= 2;
+                }
+            }
+        }
+        return perimeter;
+
+        // int [][] visited = new int[grid.length][grid[0].length];
+        // bfs(grid, 0, 0, visited);
+        // return totalCount;
     }
 
     private void bfs(int[][] grid, int i, int j, int[][] visited) {
@@ -12,7 +27,6 @@ class Solution {
 
         visited[i][j] = 1;
         if(grid[i][j] == 1) {
-            // System.out.println("inside");
             checkPerimeter(grid, i, j, visited);
         }
         bfs(grid, i+1, j, visited);
@@ -22,9 +36,7 @@ class Solution {
     }
 
     private void checkPerimeter(int [][] grid, int i, int j, int [][] visited) {
-        // if(isValid(grid, i-1, j, visited)) return;
         totalCount += 4;
-        // System.out.println("print");
 
         if(isValid(grid, i-1, j, visited) && grid[i-1][j] == 1 && visited[i-1][j] == 1) totalCount-=2;
         if(isValid(grid, i, j+1, visited) && grid[i][j+1] == 1 && visited[i][j+1] == 1) totalCount-=2;
