6e90bb86cd38c0e7e7f67b432a8ce4584abf9d90
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-04-18 00:31:49 -0300

    Time: 4 ms (99.58%) | Memory: 45.8 MB (31.17%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -15,38 +15,6 @@ class Solution {
             }
         }
         return perimeter;
-
-        // int [][] visited = new int[grid.length][grid[0].length];
-        // bfs(grid, 0, 0, visited);
-        // return totalCount;
     }
 
-    private void bfs(int[][] grid, int i, int j, int[][] visited) {
-        if(!isValid(grid, i, j, visited)) return;
-        if(visited[i][j] == 1) return;
-
-        visited[i][j] = 1;
-        if(grid[i][j] == 1) {
-            checkPerimeter(grid, i, j, visited);
-        }
-        bfs(grid, i+1, j, visited);
-        bfs(grid, i-1, j, visited);
-        bfs(grid, i, j+1, visited);
-        bfs(grid, i, j-1, visited);
-    }
-
-    private void checkPerimeter(int [][] grid, int i, int j, int [][] visited) {
-        totalCount += 4;
-
-        if(isValid(grid, i-1, j, visited) && grid[i-1][j] == 1 && visited[i-1][j] == 1) totalCount-=2;
-        if(isValid(grid, i, j+1, visited) && grid[i][j+1] == 1 && visited[i][j+1] == 1) totalCount-=2;
-
-        if(isValid(grid, i, j-1, visited) && grid[i][j-1] == 1 && visited[i][j-1] == 1) totalCount-=2;
-        if(isValid(grid, i+1, j, visited) && grid[i+1][j] == 1 && visited[i+1][j] == 1) totalCount-=2;
-    }
-
-    private boolean isValid(int [][] grid, int i, int j, int [][] visited) {
-        if(i < 0 || i>=grid.length || j <0 || j>=grid[0].length) return false;
-        return true;
-    }
 }
