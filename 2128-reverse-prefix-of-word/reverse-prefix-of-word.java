60ad117865125ad8b57627512f77f22972d5dc5f
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-01 01:14:22 -0300

    Time: 0 ms (100.00%) | Memory: 41.6 MB (56.56%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
@@ -6,21 +6,7 @@ class Solution {
         else {
             StringBuilder sb = new StringBuilder(word.substring(0, index+1));
             return sb.reverse().toString() + word.substring(index+1, n);
-            // String str = word.substring(0, index+1);
-            // // System.out.println(str);
-            // str = reverse(str);
-            // String str2 = word.substring(index+1, n);
-            // // System.out.println(str2);
-            // return str+str2;
 
         }
-    }
-
-    String reverse(String str) {
-        String res = "";
-        for(int i = str.length() - 1; i>=0;i--) {
-            res += str.charAt(i);
-        }
-        return res;
     }
 }
