2af45c77269e3795e27c915805ed76eafb2bd165
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-04 17:58:58 -0300

    Time: 1 ms (95.39%) | Memory: 45 MB (90.88%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -1,29 +1,48 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        if(n == 0) return true;
-        int i =1;
-        if(flowerbed.length >= 2) {
-            if(flowerbed[0] == flowerbed[1] && flowerbed[0] == 0) {
-                flowerbed[0] = 1;
-                n--;
-            }
-        } else if(flowerbed.length == 1 && flowerbed[0] == 0) {
-            n--;
-        }
-        while(i < flowerbed.length && n > 0) {
-            if(i == flowerbed.length - 1) {
-                if(flowerbed[i] != 1 && flowerbed[i-1] != 1) {
-                    flowerbed[i] = 1;
+        // if(n == 0) return true;
+        // int i =1;
+        // if(flowerbed.length >= 2) {
+        //     if(flowerbed[0] == flowerbed[1] && flowerbed[0] == 0) {
+        //         flowerbed[0] = 1;
+        //         n--;
+        //     }
+        // } else if(flowerbed.length == 1 && flowerbed[0] == 0) {
+        //     n--;
+        // }
+        // while(i < flowerbed.length && n > 0) {
+        //     if(i == flowerbed.length - 1) {
+        //         if(flowerbed[i] != 1 && flowerbed[i-1] != 1) {
+        //             flowerbed[i] = 1;
+        //             n--;
+        //         }
+        //     } else if(flowerbed[i] != 1 && flowerbed[i-1] != 1 && flowerbed[i+1] != 1) {
+        //         flowerbed[i] = 1;
+        //         n--;
+        //     }
+            
+        //     i++;
+        // }
+
+        // return n == 0;
+
+        for(int i=0;i<flowerbed.length;i++){
+            if(n==0)return true;
+            if(flowerbed[i]==0){
+                boolean as=false;
+                if((i+1==flowerbed.length)||(i+1<flowerbed.length&&flowerbed[i+1]==0)){
+                    as=true;
+                }
+                if(as){
+                    flowerbed[i]=1;
+                    i++;
                     n--;
                 }
-            } else if(flowerbed[i] != 1 && flowerbed[i-1] != 1 && flowerbed[i+1] != 1) {
-                flowerbed[i] = 1;
-                n--;
-            }
-            
+           }
+           else{
             i++;
+           }
         }
-
-        return n == 0;
+        return n<=0;
     }
 }
