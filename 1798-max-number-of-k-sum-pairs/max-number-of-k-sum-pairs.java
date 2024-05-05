2ff0bafcb6f4dde17ad29486bcaf48356c2b6f0c
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-05 17:00:33 -0300

    Time: 12 ms (99.83%) | Memory: 57.9 MB (16.26%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
@@ -1,22 +1,41 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
         int n = nums.length;
-        int l = 0, r = n - 1;
         int count = 0;
-        Arrays.sort(nums);
-        while(l<r) {
-            int combination = nums[l] + nums[r];
-            if(combination == k) {
+        int index = 0;
+        for(int num: nums) {
+            if(num<k) nums[index++] = num;
+        }
+
+        Arrays.sort(nums, 0, index);
+        int left = 0, right = index - 1;
+        while(left < right) {
+            int sum = nums[left] + nums[right];
+            if(sum == k) {
                 count++;
-                l++;
-                r--;
-            } else if(combination<k) {
-                l++;
-            } else {
-                r--;
-            }
+                left++;
+                right--;
+            } else if(sum <k) left++;
+            else right --;
         }
         return count;
+
+        // int l = 0, r = n - 1;
+        // int count = 0;
+        // Arrays.sort(nums);
+        // while(l<r) {
+        //     int combination = nums[l] + nums[r];
+        //     if(combination == k) {
+        //         count++;
+        //         l++;
+        //         r--;
+        //     } else if(combination<k) {
+        //         l++;
+        //     } else {
+        //         r--;
+        //     }
+        // }
+        // return count;
         // int n = nums.length;
         // int count = 0;
         // HashMap<Integer, Integer> map = new HashMap<>();
