f8a8c7d7cb8f035675c938d2a8e5bf5b23eee6e6
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-05 16:55:16 -0300

    Time: 18 ms (93.19%) | Memory: 58.5 MB (12.83%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
@@ -1,22 +1,39 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
         int n = nums.length;
+        int l = 0, r = n - 1;
         int count = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<n;i++) {
-            int difference = k - nums[i];
-            if(map.containsKey(difference)) {
+        Arrays.sort(nums);
+        while(l<r) {
+            int combination = nums[l] + nums[r];
+            if(combination == k) {
                 count++;
-                if(map.get(difference) == 1){
-                    map.remove(difference);
-                } else {
-                    map.put(difference, map.get(difference) - 1);
-                }
+                l++;
+                r--;
+            } else if(combination<k) {
+                l++;
             } else {
-                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+                r--;
             }
         }
-
         return count;
+        // int n = nums.length;
+        // int count = 0;
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int i=0;i<n;i++) {
+        //     int difference = k - nums[i];
+        //     if(map.containsKey(difference)) {
+        //         count++;
+        //         if(map.get(difference) == 1){
+        //             map.remove(difference);
+        //         } else {
+        //             map.put(difference, map.get(difference) - 1);
+        //         }
+        //     } else {
+        //         map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        //     }
+        // }
+
+        // return count;
     }
 }
