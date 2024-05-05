f23bb52171c4cef624f8448498e12e0fb6c0d88f
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-04 23:53:08 -0300

    Time: 2 ms (94.09%) | Memory: 133.8 MB (44.44%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -12,39 +12,6 @@ class Solution {
                 return true;
         }
         return false;
-        // int i=0, j=1;
-        // while(i<=j && j<nums.length) {
-        //     if(nums[i] < nums[j]) {
-        //         if(j+1<nums.length && nums[j] < nums[j+1])
-        //             return true;
-        //     }
-        //     i++;
-        //     j++;
-        // }
-        // return false;
-        // int n = nums.length;
-        // int i = 0;
-        // while(i<n) {
-        //     int start = i+1;
-        //     while(start < n) {
-        //         if(nums[i]<nums[start]) {
-        //             int end = start+1;
-        //             while(end<nums.length && nums[start]>=nums[end])
-        //                 end++;
-        //             if(end<n) return true;
-        //         }
-        //         start++;
-        //     }
-        //     // i++;
-        //     // while(start<nums.length && nums[i]>= nums[start]){
-        //     //     start++;
-        //     // }
-        //     // int end = start+1;
-        //     // while(end<nums.length && nums[start]>=nums[end])
-        //     //     end++;
-        //     // if(start < n && end < n) return true;
-        //     i++;
-        // }
-        // return false;
+        
     }
 }
