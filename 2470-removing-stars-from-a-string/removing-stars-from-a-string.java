f089407507d1f3003db6bf2191994c1daacd6553
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-05 23:28:42 -0300

    Time: 5 ms (100.00%) | Memory: 45.4 MB (86.55%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -1,19 +1,32 @@
 class Solution {
     public String removeStars(String s) {
-        Stack<Character> characterStack = new Stack<>();
-        for(char c : s.toCharArray()){
-            if(c == '*') {
-                characterStack.pop();
-            }
-            else {
-                characterStack.push(c);
+        int index = 0;
+        int len = s.length();
+        byte[] bytes = new byte[len];
+        s.getBytes(0, len, bytes, 0);
+        for(byte ch : bytes) {
+            if(ch == '*')
+            {
+                index--;
+            }else {
+                bytes[index++] = ch;
             }
         }
+        return new String(bytes, 0, index);
+        // Stack<Character> characterStack = new Stack<>();
+        // for(char c : s.toCharArray()){
+        //     if(c == '*') {
+        //         characterStack.pop();
+        //     }
+        //     else {
+        //         characterStack.push(c);
+        //     }
+        // }
 
-        StringBuilder sb = new StringBuilder();
-        while(!characterStack.isEmpty()) {
-            sb.insert(0, characterStack.pop());
-        }
-        return sb.toString();
+        // StringBuilder sb = new StringBuilder();
+        // while(!characterStack.isEmpty()) {
+        //     sb.insert(0, characterStack.pop());
+        // }
+        // return sb.toString();
     }
 }
