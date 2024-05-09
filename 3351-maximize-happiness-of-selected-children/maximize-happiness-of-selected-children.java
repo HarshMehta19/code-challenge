7d6fe0810ee7f7b9fb7c3f4d089fa7a4d9dc23c5
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-08 21:37:55 -0300

    Time: 34 ms (97.44%) | Memory: 61.6 MB (43.21%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.java
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.java
@@ -1,9 +1,6 @@
 class Solution {
     public long maximumHappinessSum(int[] happiness, int k) {
-        // PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b-a);
-        // for(int num : happiness) {
-        //     pq.add(num);
-        // }
+
         Arrays.sort(happiness);
         
         long maxCount = 0;
@@ -12,14 +9,9 @@ class Solution {
         while(operations < k && (length - 1)- operations >=0 ) {
             if(happiness[length - operations - 1] - operations <= 0)
                 break;
-            // int pollItem = pq.poll();
-            // System.out.println("pollItem: "+ pollItem);
             maxCount+= happiness[length - operations - 1] - operations;
             operations++;
         }
-
-        // System.out.println(pq.peek());
-        // System.out.println(pq.peek());
         return maxCount;
     }
 }
