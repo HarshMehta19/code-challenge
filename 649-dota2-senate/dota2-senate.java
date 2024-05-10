11b3ae8e6876c0df6e0e8213d2cfa957e3571c6b
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-10 01:14:53 -0300

    Time: 1 ms (100.00%) | Memory: 42.5 MB (90.07%) - LeetSync

--- 649-dota2-senate/dota2-senate.java
+++ 649-dota2-senate/dota2-senate.java
@@ -1,29 +1,58 @@
 class Solution {
     public String predictPartyVictory(String senate) {
-        int length = senate.length();
         char[] charArr = senate.toCharArray();
-        Queue<Integer> D = new LinkedList<>();
-        Queue<Integer> R = new LinkedList<>();
-        int i =0;
-        while(i<length) {
-            if(charArr[i] == 'R')
-                R.add(i);
-            else
-                D.add(i);
-            i++;
+        return solve(charArr, 0);
+        // int length = senate.length();
+        // char[] charArr = senate.toCharArray();
+        // Queue<Integer> D = new LinkedList<>();
+        // Queue<Integer> R = new LinkedList<>();
+        // int i =0;
+        // while(i<length) {
+        //     if(charArr[i] == 'R')
+        //         R.add(i);
+        //     else
+        //         D.add(i);
+        //     i++;
+        // }
+
+        // while(!R.isEmpty() && !D.isEmpty()) {
+        //     int iR = R.poll();
+        //     int iD = D.poll();
+
+        //     if(iR < iD)
+        //         R.add(iR + length);
+        //     else
+        //         D.add(iD + length);
+        // }
+
+        // return R.isEmpty() ? "Dire" : "Radiant";
+
+    }
+
+    private String solve(char[] charArr, int vote) {
+        int d = 0, r = 0;
+        int index = 0;
+        while(index < charArr.length) {
+            if(charArr[index] == 'D') {
+                if(vote>=0){
+                    d++;
+                } else {
+                    charArr[index] = '0';
+                }
+                vote++;
+            } else if(charArr[index] == 'R') {
+                if(vote <= 0) {
+                    r++;
+                } else {
+                    charArr[index] = '0';
+                }
+                vote--;
+            }
+            index++;
         }
+        if(d!=0 && r==0) return "Dire";
+        if(r!=0 && d==0) return "Radiant";
 
-        while(!R.isEmpty() && !D.isEmpty()) {
-            int iR = R.poll();
-            int iD = D.poll();
-
-            if(iR < iD)
-                R.add(iR + length);
-            else
-                D.add(iD + length);
-        }
-
-        return R.isEmpty() ? "Dire" : "Radiant";
-
+        return solve(charArr, vote);
     }
 }
