d0e70d02b6635f849beb0ec5c734589d637da411
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-10 16:06:05 -0300

    Time: 3 ms (100.00%) | Memory: 63.7 MB (46.55%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -10,21 +10,46 @@
  */
 class Solution {
     public int pairSum(ListNode head) {
-        int i =0;
-        ListNode node = head;
-        HashMap<Integer, Integer> map = new HashMap<>();
 
-        while(node != null) {
-            map.put(i, node.val);
-            i++;
-            node = node.next;
+        // Space complexity o(n) and time is also O(n)
+        // int i =0;
+        // ListNode node = head;
+        // HashMap<Integer, Integer> map = new HashMap<>();
+
+        // while(node != null) {
+        //     map.put(i, node.val);
+        //     i++;
+        //     node = node.next;
+        // }
+        // int result = -1;
+        // int start = 0;
+        // while(start <= (i/2 - 1)){
+        //     result = Math.max(map.get(start) + map.get(i-start-1), result);
+        //     start++;
+        // }
+        // return result;
+
+        // Optimal solution
+
+        ListNode slow = head;
+        ListNode prev = new ListNode(0);
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+
+            ListNode temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+
+            slow=temp;
         }
-        // System.out.println("i is: "+ i);
+
         int result = -1;
-        int start = 0;
-        while(start <= (i/2 - 1)){
-            result = Math.max(map.get(start) + map.get(i-start-1), result);
-            start++;
+        while(slow != null) {
+            result = Math.max(result, slow.val + prev.val);
+            slow = slow.next;
+            prev = prev.next;
         }
         return result;
     }
