74c278e3eab4df0d5d0be158b427e37f548ac314
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-09 21:19:49 -0300

    Time: 19 ms (96.98%) | Memory: 52.7 MB (90.63%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.java
+++ 969-number-of-recent-calls/number-of-recent-calls.java
@@ -1,15 +1,22 @@
 class RecentCounter {
-    Queue<Integer> q;
+    // Queue<Integer> q;
+    private int[] q = new int[10000];
+    int start = 0, end = 0;
     public RecentCounter() {
-        q = new LinkedList<>();
+        start = 0;
+        end = 0;
     }
     
     public int ping(int t) {
         // System.out.println("size: "+ q.size());
-        q.add(t);
-        while(q.peek() < t - 3000)
-            q.poll();
-        return q.size();
+        while(start<end && (t - q[start] > 3000))
+            start++;
+        q[end++] = t;
+        return end - start;
+        // q.add(t);
+        // while(q.peek() < t - 3000)
+        //     q.poll();
+        // return q.size();
         
     }
 }
