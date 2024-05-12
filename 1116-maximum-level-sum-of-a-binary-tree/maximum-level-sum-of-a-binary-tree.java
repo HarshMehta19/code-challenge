2d124984c05b28432d8d4a677aafdd780143bc55
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-11 21:18:41 -0300

    Time: 4 ms (98.58%) | Memory: 46.5 MB (89.16%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.java
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.java
@@ -16,16 +16,16 @@
 class Solution {
     int sumResult = Integer.MIN_VALUE;
     int level = 0;
+    int maxLevel = 0;
     int[] arr;
     public int maxLevelSum(TreeNode root) {
         arr = new int[10001];
         Arrays.fill(arr, Integer.MIN_VALUE);
         bfs(root, 1);
 
-        for(int i = 1;i<10001;i++) {
+        for(int i = 1;i<=maxLevel;i++) {
             if(sumResult < arr[i]) {
                 sumResult = arr[i];
-                // System.out.println("sumResult: "+ sumResult + ", i is: "+ i);
                 level = i;
             }
         }
@@ -42,5 +42,6 @@ class Solution {
 
         bfs(root.left, level+1);
         bfs(root.right, level+1);
+        maxLevel = Math.max(maxLevel, level+1);
     }
 }
