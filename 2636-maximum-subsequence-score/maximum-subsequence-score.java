3cb4089592ecfc995923f0e67c041f184ea8c4ab
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-18 19:11:24 -0300

    Time: 79 ms (50.93%) | Memory: 61.8 MB (83.12%) - LeetSync

--- 2636-maximum-subsequence-score/maximum-subsequence-score.java
+++ 2636-maximum-subsequence-score/maximum-subsequence-score.java
@@ -16,16 +16,7 @@ class Solution {
             if(pq.size() > k) sum -= pq.poll();
             if(pq.size() == k) res = Math.max(res, (sum * element[0]));
         }
-        // Arrays.sort(elements, (a,b)-> b[0] - a[0]);
-        // ArrayList<Integer> list = new ArrayList<>();
-        // long res = 0, sum = 0;
-        // for(int[] element : elements) {
-        //     list.add(element[1]);
-        //     sum+= element[1];
-
-        //     if(list.size() > k) sum -= list.remove(0);
-        //     if(list.size() == k) res = Math.max(res, (sum * element[0]));
-        // }
+        
         return res;
     }
 }
