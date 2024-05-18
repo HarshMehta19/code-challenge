aa0424e0e11865c001e0fc5e0963ab7f22758dcf
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-18 17:05:41 -0300

    Time: 9 ms (95.69%) | Memory: 45.2 MB (64.22%) - LeetSync

--- 2413-smallest-number-in-infinite-set/smallest-number-in-infinite-set.java
+++ 2413-smallest-number-in-infinite-set/smallest-number-in-infinite-set.java
@@ -1,28 +1,55 @@
 class SmallestInfiniteSet {
-    int cur;
-    Set<Integer> s;
 
-    public SmallestInfiniteSet() {
-        cur = 1;
-        s = new HashSet<>();
+    byte nums[];
+    int smallest=1;
+     SmallestInfiniteSet() {
+       nums = new byte[1001];
     }
     
     public int popSmallest() {
-        if(!s.isEmpty()) {
-            int res = Collections.min(s);
-            s.remove(res);
-            return res;
-        } else {
-            cur++;
-            return cur - 1;
+        for(int i = smallest;i<1001;i++){
+            if(nums[i]!=1){
+                smallest = i;
+                nums[i]=1;
+                break;
+            }
         }
+        return smallest;
     }
     
     public void addBack(int num) {
-        if(cur > num) {
-            s.add(num);
+         if(nums[num]==1){
+            nums[num]=0;
+        }
+        if(smallest>num){
+            smallest=num;
         }
     }
+
+    // int cur;
+    // Set<Integer> s;
+
+    // public SmallestInfiniteSet() {
+    //     cur = 1;
+    //     s = new HashSet<>();
+    // }
+    
+    // public int popSmallest() {
+    //     if(!s.isEmpty()) {
+    //         int res = Collections.min(s);
+    //         s.remove(res);
+    //         return res;
+    //     } else {
+    //         cur++;
+    //         return cur - 1;
+    //     }
+    // }
+    
+    // public void addBack(int num) {
+    //     if(cur > num) {
+    //         s.add(num);
+    //     }
+    // }
 }
 
 /**
