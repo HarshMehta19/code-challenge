54b720700a19e6531f59eed086fbc479ab83ff34
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-18 19:14:28 -0300

    Time: 78 ms (57.96%) | Memory: 61.9 MB (83.12%) - LeetSync

--- 2636-maximum-subsequence-score/maximum-subsequence-score.java
+++ 2636-maximum-subsequence-score/maximum-subsequence-score.java
@@ -1,6 +1,14 @@
 class Solution {
     public long maxScore(int[] nums1, int[] nums2, int k) {
         int n = nums1.length;
+        long res = 0, sum = 0;
+
+        if(k == 1) {
+            for(int i = 0;i<n;i++) {
+                res = Math.max(res, (nums1[i] * nums2[i]));
+            }
+            return res;
+        }
         int[][] elements = new int[n][2];
         for(int i=0;i<n;i++) {
             elements[i] = new int[]{nums2[i], nums1[i]};
@@ -8,7 +16,6 @@ class Solution {
 
         Arrays.sort(elements, (a,b)-> b[0] - a[0]);
         PriorityQueue<Integer> pq = new PriorityQueue<>(k);
-        long res = 0, sum = 0;
         for(int[] element : elements) {
             pq.add(element[1]);
             sum+= element[1];
