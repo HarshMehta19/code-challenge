0e6b865b224db2bbae46b2040c48856e2bae1443
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-19 12:29:39 -0300

    Time: 21 ms (94.54%) | Memory: 55 MB (99.30%) - LeetSync

--- 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.java
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.java
@@ -1,33 +1,67 @@
 class Solution {
     public long totalCost(int[] costs, int k, int candidates) {
-        int i = 0;
-        int j = costs.length - 1;
-        PriorityQueue<Integer> pq1 = new PriorityQueue<>();
-        PriorityQueue<Integer> pq2 = new PriorityQueue<>();
+        // int i = 0;
+        // int j = costs.length - 1;
+        // PriorityQueue<Integer> pq1 = new PriorityQueue<>();
+        // PriorityQueue<Integer> pq2 = new PriorityQueue<>();
 
-        long ans = 0;
-        while (k-- > 0) {
-            while (pq1.size() < candidates && i <= j) {
-                // System.out.println("i is: "+ i);
-                pq1.offer(costs[i++]);
-            }
-            while (pq2.size() < candidates && i <= j) {
-                // System.out.println("j is: "+ j);
+        // long ans = 0;
+        // while (k-- > 0) {
+        //     while (pq1.size() < candidates && i <= j) {
+        //         // System.out.println("i is: "+ i);
+        //         pq1.offer(costs[i++]);
+        //     }
+        //     while (pq2.size() < candidates && i <= j) {
+        //         // System.out.println("j is: "+ j);
                 
-                pq2.offer(costs[j--]);
+        //         pq2.offer(costs[j--]);
+        //     }
+
+        //     int t1 = pq1.size() > 0 ? pq1.peek() : Integer.MAX_VALUE;
+        //     int t2 = pq2.size() > 0 ? pq2.peek() : Integer.MAX_VALUE;
+
+        //     if (t1 <= t2) {
+        //         ans += t1;
+        //         pq1.poll();
+        //     } else {
+        //         ans += t2;
+        //         pq2.poll();
+        //     }
+        // }
+        // return ans;
+        int n = costs.length;
+        long cost = 0;
+
+        if (candidates * 2 + k > n) {
+            Arrays.sort(costs);
+            for (int i = 0; i < k; i++) {
+                cost += costs[i];
             }
+            return cost;
+        }
+        // return 0;
+        PriorityQueue<Integer> pre = new PriorityQueue<>();
+        PriorityQueue<Integer> post = new PriorityQueue<>();
 
-            int t1 = pq1.size() > 0 ? pq1.peek() : Integer.MAX_VALUE;
-            int t2 = pq2.size() > 0 ? pq2.peek() : Integer.MAX_VALUE;
+        for(int i =0;i<candidates;i++) {
+            pre.offer(costs[i]);
+            post.offer(costs[n - 1 - i]);
+        }
+        int i= candidates, j = n - 1 - candidates;
 
-            if (t1 <= t2) {
-                ans += t1;
-                pq1.poll();
+        while(k-- > 0) {
+            if(pre.peek() <= post.peek()) {
+                cost += pre.poll();
+                pre.offer(costs[i++]);
             } else {
-                ans += t2;
-                pq2.poll();
+                cost += post.poll();
+                post.offer(costs[j--]);
             }
         }
-        return ans;
+
+        return cost;
+
+
+
     }
 }
