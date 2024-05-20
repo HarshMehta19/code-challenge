6455015894b575c333cd2c997c835b9b3d7ac44b
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-20 12:30:05 -0300

    Time: 0 ms (100.00%) | Memory: 40 MB (75.08%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n==0) return 0;
+        if(n<=2) return 1;
+        // int []dp = new int[n+1];
+        // Arrays.fill(dp, -1);
+        // return recursion(n, dp);
+        return solve(n);
+    }
+    
+    // tablulation
+    int solve(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 0; dp[1] = 1; dp[2] = 1;
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
+            // System.out.println("i, dp[i]: "+ i + ", "+ dp[i] );
+        }
+        return dp[n];
+    }
+
+    // Memoization
+    int recursion(int n, int[] dp) {
+        
+        if(n <= 0) return 0;
+        else if(n<=2) return 1;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = recursion(n-3, dp) + recursion(n-2, dp) + recursion(n-1, dp);
+    }
+}
