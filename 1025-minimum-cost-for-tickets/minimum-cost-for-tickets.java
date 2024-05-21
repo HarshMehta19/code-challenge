c78f275d9087796cc07d78d9d8d4593644d33a3e
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-21 19:53:24 -0300

    Time: 0 ms (100.00%) | Memory: 41.3 MB (61.16%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -9,6 +9,30 @@ class Solution {
         // return solve(days, costs, 0);
     }
 
+    int solveTab(int[] days, int[] costs, int index) {
+        int[] dp = new int[days.length+1];
+
+        dp[days.length] = 0;
+
+        for(int k = days.length-1;k>=0;k++) {
+            int option1 = costs[0] + solveMem(days, costs, index+1, dp);
+        
+            // 7 Days pass
+            int i;
+            for(i = k;i< days.length && days[i] < days[k] + 7;i++);
+
+            int option2 = costs[1] + solveMem(days, costs, i, dp);
+
+            // 30 days pass
+            for(i = k;i<days.length && days[i] < days[k] + 30; i++);
+
+            int option3 = costs[2] + solveMem(days, costs, i, dp);
+
+            dp[k] = Math.min(option1, Math.min(option2, option3));
+        }
+        return dp[0];
+    }
+
     int solveMem(int[] days, int[] costs, int index, int[] dp) {
         if(index >= days.length) return 0;
 
