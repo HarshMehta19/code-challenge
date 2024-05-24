5945bec98a216a8a6b4b13e01f5c7ab9a499f96a
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-24 19:38:15 -0300

    Time: 4 ms (48.84%) | Memory: 40.9 MB (67.77%) - LeetSync

--- 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.java
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.java
@@ -3,19 +3,36 @@ class Solution {
         int n = values.length;
 
         // Tabulation
-        int[][] dp = new int[n][n];
-        return solveTab(values, 0, n-1, dp);
+        return solveTab(values);
+        // Memoization
+        // int[][] dp = new int[n][n];
+        // return solveMem(values, 0, n-1, dp);
 
         // Recursion
         // return solve(values, 0, n-1);
     }
 
-    private int solveTab(int[] values, int start, int end, int[][] dp) {
+    private int solveTab(int[] values) {
+        int n = values.length;
+        int[][] dp = new int[n][n];
+        for(int i = n-1;i>=0;i--) {
+            for(int j=i+2;j<n;j++) {
+                int ans = Integer.MAX_VALUE;
+                for(int k = i+1;k<j; k++) {
+                    ans = Math.min(ans, values[i]*values[k]*values[j] + dp[i][k] + dp[k][j]);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][n-1];
+    }
+
+    private int solveMem(int[] values, int start, int end, int[][] dp) {
         if(start + 1 == end) return 0;
         int ans = Integer.MAX_VALUE;
         if(dp[start][end] != 0) return dp[start][end];
         for(int i = start+1;i<end; i++) {
-            ans = Math.min(ans, values[i]*values[start]*values[end] + solveTab(values, start, i, dp) + solveTab(values, i, end, dp));
+            ans = Math.min(ans, values[i]*values[start]*values[end] + solveMem(values, start, i, dp) + solveMem(values, i, end, dp));
         }
         return dp[start][end] = ans;
     }
