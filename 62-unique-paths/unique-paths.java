eb33949e2e44142e1837a2c941e93ff43e90e075
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2024-05-26 02:11:06 -0300

    Time: 0 ms (100.00%) | Memory: 40.6 MB (21.56%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,14 +1,31 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        // int[][] maze = new int[m][n];
+        
+        //Tabulation
+        return solveTab(m, n);
+
+        // Memoization
+        // int[][] dp = new int[m][n];
+        // for (int i = 0; i < m; ++i) { // init
+        //     for (int j = 0; j < n; ++j) {
+        //         dp[i][j] = -1;
+        //     }
+        // }
+        // return solveMem(m,n, 0, 0, dp);
+        // return solve(m, n, 0, 0);
+    }
+
+    private int solveTab(int m, int n) {
         int[][] dp = new int[m][n];
-        for (int i = 0; i < m; ++i) { // init
-            for (int j = 0; j < n; ++j) {
-                dp[i][j] = -1;
+        for (int i = 0; i < m; ++i) dp[i][n-1] = 1;
+  for (int i = 0; i < n; ++i) dp[m-1][i] = 1;
+        for(int row = m-2;row>=0;row--) {
+            for(int col = n-2;col>=0;col--) {
+                dp[row][col] = dp[row+1][col] + dp[row][col+1];
             }
         }
-        return solveMem(m,n, 0, 0, dp);
-        // return solve(m, n, 0, 0);
+        return dp[0][0];
+
     }
 
     private int solveMem(int m, int n, int row, int col, int[][] dp) {
