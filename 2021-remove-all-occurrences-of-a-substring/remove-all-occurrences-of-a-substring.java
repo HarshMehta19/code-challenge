cac950dbc3a315eb07e7ef477fd990619422cfa1
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2025-02-10 21:05:38 -0400

    Time: 8 ms (30.12%) | Memory: 45.2 MB (5.62%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -1,12 +1,29 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        StringBuilder sb = new StringBuilder(s);
+        
+        if(s.equals(part)) return "";
+        int m = s.length(), n = part.length();
+        if(m<n) return s;
 
-        while(sb.indexOf(part) != -1) {
-            int i = sb.indexOf(part);
-            sb.delete(i, i + part.length());
+        int i=0, j=0;
+
+        while(i<s.length() && j < s.length()) {
+            if(j-i + 1 < n)
+                j++;
+            else {
+                String subStr = s.substring(i, j+1);
+                if(subStr.equals(part))
+                {
+                    s = s.substring(0, i) + s.substring(j+1);
+                    i=0;
+                    j=0;
+                } else {
+                    i++;
+                    j++;
+                }
+            }
         }
 
-        return sb.toString();
+        return s;
     }
 }
