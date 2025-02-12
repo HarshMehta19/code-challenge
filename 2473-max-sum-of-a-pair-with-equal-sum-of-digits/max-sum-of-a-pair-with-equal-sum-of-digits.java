b02567313bc5c9bb6d45f73dd52dbf6ca0aeb654
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2025-02-11 22:42:44 -0400

    Time: 19 ms (99.65%) | Memory: 55.5 MB (85.94%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,26 +1,18 @@
 class Solution {
     public int maximumSum(int[] nums) {
-        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
-        for(int num: nums) {
-            int copyNum = num;
-            int sum = 0;
-            while(copyNum != 0) {
-                sum += copyNum % 10;
-                copyNum = copyNum / 10;
-            }
-
-            map.putIfAbsent(sum, new ArrayList<>());
-
-            map.get(sum).add(num);
-        }
-
+        int[] sumNum = new int[100];
         int ans = -1;
-        for(Map.Entry<Integer, ArrayList<Integer>> entry : map.entrySet()) {
-            ArrayList<Integer> list = entry.getValue();
-            if(list.size() >= 2 ) {
-                Collections.sort(list);
-                ans = Math.max(ans, list.get(list.size() - 1) + list.get(list.size() - 2));
+        for(int num : nums) {
+            int tempNum = num;
+            int sum = 0;
+            while(tempNum != 0 ){
+                sum += tempNum % 10;
+                tempNum /= 10;
             }
+            if(sumNum[sum] != 0)
+                ans = Math.max(ans, num + sumNum[sum]);
+            
+            sumNum[sum] = Math.max(sumNum[sum], num);
         }
 
         return ans;
