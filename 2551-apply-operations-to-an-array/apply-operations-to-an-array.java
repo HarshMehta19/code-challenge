44060a94aac12a07dd7fbf29653e72351fd56627
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2025-03-01 16:57:28 -0400

    Time: 1 ms (96.52%) | Memory: 44.4 MB (12.22%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
@@ -8,6 +8,7 @@ class Solution {
             if (nums[i] == nums[i + 1]) {
                 nums[i] = nums[i] * 2;
                 nums[i + 1] = 0;
+                i++;
             }
         }
 
