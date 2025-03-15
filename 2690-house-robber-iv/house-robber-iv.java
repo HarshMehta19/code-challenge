68a8aa1c3d6988165661912384b4dde6a7fc77e1
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2025-03-15 00:13:03 -0300

    Time: 15 ms (100.00%) | Memory: 55.6 MB (89.05%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.java
+++ 2690-house-robber-iv/house-robber-iv.java
@@ -1,16 +1,19 @@
 class Solution {
     public int minCapability(int[] nums, int k) {
-        int max = 1;
-        for(int num : nums)
+        int max = 1, min = Integer.MAX_VALUE;
+        for (int num : nums) {
             max = Math.max(max, num);
-        int left = 1, right = max, n = nums.length;
+            min = Math.min(min, num);
+        }
+        int left = min, right = max, n = nums.length;
         while (left < right) {
             int mid = (left + right) / 2, take = 0;
-            for (int i = 0; i < n; ++i)
+            for (int i = 0; i < n; i++) {
                 if (nums[i] <= mid) {
                     take += 1;
                     i++;
                 }
+            }
             if (take >= k)
                 right = mid;
             else
