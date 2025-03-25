42f62d692f681612aa4d638ab656e5b88d320a0b
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2025-03-25 00:45:58 -0300

    Time: 121 ms (27.63%) | Memory: 143 MB (8.77%) - LeetSync

---
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean checkValidCuts(int n, int[][] rectangles) {
+        ArrayList<int[]> xCoord = new ArrayList<>();
+        ArrayList<int[]> yCoord = new ArrayList<>();
+
+        for(int[]rectangle: rectangles) {
+            xCoord.add(new int[]{rectangle[0], rectangle[2]});
+            yCoord.add(new int[]{rectangle[1], rectangle[3]});
+        }
+
+        Collections.sort(xCoord, (a, b) -> Integer.compare(a[0], b[0]));
+        Collections.sort(yCoord, (a, b) -> Integer.compare(a[0], b[0]));
+
+        return Math.max(countNonOverlapping(xCoord), countNonOverlapping(yCoord)) >= 3;
+    }
+
+    private int countNonOverlapping(ArrayList<int[]> intervals) {
+        int sections = 1;
+        int maxEnd = intervals.get(0)[1];
+
+        for(int i=1;i<intervals.size();i++) {
+            if(maxEnd <= intervals.get(i)[0])
+                sections++;
+            maxEnd = Math.max(maxEnd, intervals.get(i)[1]);
+        }
+
+        return sections;
+    }
+}
