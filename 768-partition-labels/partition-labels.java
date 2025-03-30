1fb720ac4de9d8ee55baf350a00d76b8354258e1
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2025-03-30 00:26:06 -0300

    Time: 3 ms (97.31%) | Memory: 42.3 MB (51.26%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -1,17 +1,18 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
         int[] lastPos = new int[26];
+        char[] arr = s.toCharArray();
         for (int i = 0; i < s.length(); i++) {
-            lastPos[s.charAt(i) - 'a'] = i;
+            lastPos[arr[i] - 'a'] = i;
         }
 
         List<Integer> result = new ArrayList<>();
 
         int size = 0, end = 0;
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < arr.length; i++) {
             size++;
 
-            end = Math.max(end, lastPos[s.charAt(i) - 'a']);
+            end = Math.max(end, lastPos[arr[i] - 'a']);
             if (end == i) {
                 result.add(size);
                 size = 0;
