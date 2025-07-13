bc0cfcb62d2aca2618dfbe4b09dec842018ac60b
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2025-07-13 14:53:34 -0300

    Time: 1 ms (39.83%) | Memory: 17.6 MB (89.30%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -12,8 +12,8 @@ class Solution:
             one_digit = int(s[i - 1])
             two_digit = int(s[i-2:i])
 
-            if one_digit != 0:
-                dp[i] += dp[i-1]
+            if 1 <= one_digit <= 9:
+                dp[i] += dp[i - 1]
 
             if 10 <= two_digit <= 26:
                 dp[i] += dp[i-2]
