372100b5b210bc0f539fbf2d583a01fcacba82dc
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2025-07-13 16:58:41 -0300

    Time: 2085 ms (77.36%) | Memory: 18.2 MB (39.73%) - LeetSync

--- 279-perfect-squares/perfect-squares.py
+++ 279-perfect-squares/perfect-squares.py
@@ -3,11 +3,9 @@ class Solution:
         dp = [float('inf')] * (n + 1)
         dp[0] = 0
         
-        for target in range(1, n+1):
-            for s in range(1, target + 1):
-                square = s * s
-                if target - square < 0:
-                    break
-                dp[target] = min(dp[target], 1 + dp[target-square])
+        for target in range(1, int(sqrt(n))+1):
+            target = target**2
+            for j in range(target, n+1):
+                dp[j] = min(dp[j], 1 + dp[j-target])
 
-        return dp[target]
+        return dp[n]
