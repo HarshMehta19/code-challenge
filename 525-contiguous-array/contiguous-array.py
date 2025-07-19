e25e7d56d03add1310ecf9701f11b6c6d9479f08
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2025-07-19 00:06:31 -0300

    Time: 123 ms (15.12%) | Memory: 23.5 MB (21.97%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,5 +1,9 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
+        # check total number of zeroes and ones till the position
+        # set difference of ones and zeroes in the map with their index
+        # at any step, if ones and zeroes are same, set the result
+        # If difference is already exists in map that means prefix is not required
         zero = one = 0
         res = 0
 
