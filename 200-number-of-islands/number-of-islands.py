19c0b518032c2174dd2b6a52e68bb18bc2936d77
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2025-07-21 19:40:05 -0300

    Time: 240 ms (70.28%) | Memory: 20 MB (82.29%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -4,31 +4,23 @@ class Solution:
         if not grid:
             return 0
 
-        rowLen, colLen = len(grid), len(grid[0])
-        num_islands = 0
-
-
-
-        def dfs(row: int, col: int) -> None:
-
-            if row < 0 or row >= rowLen or col < 0 or col >= colLen or grid[row][col] != '1':
+        rows, cols = len(grid), len(grid[0])
+        res = 0
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c]!= '1':
                 return
 
-            grid[row][col] = 0
+            grid[r][c] = 0
 
-            dfs(row+1, col)
-            
-            dfs(row-1, col)
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
 
-            dfs(row, col + 1)
-
-            dfs(row, col - 1)
-        
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        for i in range(rows):
+            for j in range(cols):
                 if grid[i][j] == '1':
-                    num_islands += 1
+                    res+=1
                     dfs(i, j)
 
-        return num_islands
-        
+        return res
