867e8385dd97017809ad008f293bbc1708802aaa
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2025-07-29 00:48:20 -0300

    Time: 283 ms (64.66%) | Memory: 18.1 MB (60.42%) - LeetSync

--- 2325-number-of-ways-to-select-buildings/number-of-ways-to-select-buildings.py
+++ 2325-number-of-ways-to-select-buildings/number-of-ways-to-select-buildings.py
@@ -1,16 +1,27 @@
 class Solution:
     def numberOfWays(self, s: str) -> int:
-        zeros = s.count("0")
-
-        ones = len(s) - zeros
-
-        zeroPrefix = onePrefix = res = 0
-        for c in s:
-            if c == "0":
-                res += onePrefix * (ones - onePrefix)
-                zeroPrefix+=1
+        n = len(s)
+        
+        # Count total 0s and 1s
+        total_zeros = s.count('0')
+        total_ones = n - total_zeros
+        
+        result = 0
+        zeros_seen = 0
+        ones_seen = 0
+        
+        for i in range(n):
+            if s[i] == '0':
+                # Current is '0', so we need '1' on both sides for valid triplet
+                # ones_seen = 1s to the left
+                # (total_ones - ones_seen) = 1s to the right
+                result += ones_seen * (total_ones - ones_seen)
+                zeros_seen += 1
             else:
-                res += zeroPrefix * (zeros - zeroPrefix)
-                onePrefix +=1
-
-        return res
+                # Current is '1', so we need '0' on both sides for valid triplet
+                # zeros_seen = 0s to the left  
+                # (total_zeros - zeros_seen) = 0s to the right
+                result += zeros_seen * (total_zeros - zeros_seen)
+                ones_seen += 1
+        
+        return result
