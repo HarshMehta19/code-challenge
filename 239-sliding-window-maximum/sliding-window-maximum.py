27d6b6b4772cd74c99abd00c710825f300316b26
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2025-07-30 00:09:45 -0300

    Time: 194 ms (30.20%) | Memory: 32.1 MB (24.00%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -7,11 +7,11 @@ class Solution:
             
             while q and nums[q[-1]] < nums[r]:
                 q.pop()
-            q.append(r)
 
-            if l > q[0]:
+            if q and l > q[0]:
                 q.popleft()
                 
+            q.append(r)
             if r+1 >= k:
                 res.append(nums[q[0]])
                 l+=1
