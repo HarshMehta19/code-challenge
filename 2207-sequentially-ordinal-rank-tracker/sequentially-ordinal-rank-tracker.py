20abec94aa1aa53d86c29a81988b6cc39aa9eebf
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2025-08-03 01:06:55 -0300

    Time: 251 ms (89.71%) | Memory: 40.9 MB (89.30%) - LeetSync

--- 2207-sequentially-ordinal-rank-tracker/sequentially-ordinal-rank-tracker.py
+++ 2207-sequentially-ordinal-rank-tracker/sequentially-ordinal-rank-tracker.py
@@ -1,18 +1,37 @@
+class FrequencyMinHeap:
+    def __init__(self, score, name):
+        self.name = name
+        self.score = score
+
+    def __lt__(self, other):
+        if other.score != self.score:
+            return self.score < other.score
+        return self.name > other.name
+
+    def __repr__(self):
+        return f'({self.score}, {self.name})'
+
 class SORTracker:
 
     def __init__(self):
-        self.queryCount = 0
-        self.storage = SortedList()
+        self.minHeap = []
+        self.maxHeap = []
         
 
     def add(self, name: str, score: int) -> None:
-        self.storage.add((-score, name))
+        heapq.heappush(self.minHeap, FrequencyMinHeap(score, name))
+        minValue = heapq.heappop(self.minHeap)
+        heapq.heappush(self.maxHeap, (-minValue.score, minValue.name))
+
+        # print("add", self.minHeap, "maxHeap", self.maxHeap)
 
     def get(self) -> str:
-        ans = self.storage[self.queryCount]
-        self.queryCount+=1
-        # print(self.storage)
-        return ans[1]
+        if self.maxHeap:
+            maxValue = heapq.heappop(self.maxHeap)
+            heapq.heappush(self.minHeap, FrequencyMinHeap(-maxValue[0], maxValue[1]))
+        # print(self.minHeap, self.maxHeap)
+        
+        return self.minHeap[0].name
         
 
 
