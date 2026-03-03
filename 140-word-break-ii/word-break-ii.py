4df5596f64b0eb5caf0292af283d9b992820a31a
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2026-03-02 21:07:53 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (18.64%) - LeetSync

--- 140-word-break-ii/word-break-ii.py
+++ 140-word-break-ii/word-break-ii.py
@@ -8,14 +8,17 @@ class Solution:
 
             if i in memo:
                 return memo[i]
+
             res = []
-            for j in range(i,len(s)):
+
+            for j in range(i, len(s)):
                 w = s[i:j+1]
 
                 if w not in wordDict:
                     continue
                 
                 strings = backtrack(j+1)
+
                 if not strings:
                     continue
                 
@@ -24,10 +27,7 @@ class Solution:
                     if substr:
                         sentence += " " + substr
                     res.append(sentence)
+            
             memo[i] = res
             return res
-
-        # cur = []
-        # res = []
-        return backtrack(0)
-        # return res
+        return  backtrack(0)
