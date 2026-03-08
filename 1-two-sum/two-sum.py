2ca6966fcf819e5439db5e738aaadd29be2883e4
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2026-03-08 00:21:38 -0400

    Time: 2 ms (55.52%) | Memory: 20.5 MB (58.99%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -4,9 +4,8 @@ class Solution:
 
         for i in range(len(nums)):
             num = nums[i]
-            if target - num in map:
+            if map and target - num in map:
                 return [map[target - num], i]
-
             map[num] = i
-
         return None
+
