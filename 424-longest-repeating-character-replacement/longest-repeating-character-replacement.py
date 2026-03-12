e19c44c78e76a2f98446cdcfd304c5196516912e
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2026-03-12 18:30:33 -0300

    Time: 79 ms (76.97%) | Memory: 19.8 MB (46.13%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,40 +1,15 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count = {}
+        map = {}
         left, max_freq, res = 0, 0, 0
-
         for idx, value in enumerate(s):
-            count[value] = count.get(value, 0) + 1
-            max_freq = max(max_freq, count[value])
+            map[value] = map.get(value, 0) + 1
+            max_freq = max(max_freq, map[value])
 
             if (idx - left + 1) - max_freq > k:
-                count[s[left]]-=1
+                map[s[left]]-=1
                 left+=1
-            
+
             res = max(res, idx - left + 1)
-        
+
         return res
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # count = {}
-        # l = max_freq = res = 0
-
-        # for idx, value in enumerate(s):
-        #     count[value] = count.get(value, 0) + 1
-        #     max_freq = max(count[value], max_freq)
-
-        #     if (idx - l + 1) - max_freq > k:
-        #         count[s[l]]-=1
-        #         l+=1
-
-        #     res = max(res, (idx - l + 1))
-
-        # return res
