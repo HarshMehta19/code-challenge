16bd506f74fc36dac2cc242f9e3fe767486015ba
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2026-03-12 00:46:34 -0300

    Time: 15 ms (42.78%) | Memory: 19.3 MB (91.90%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -6,10 +6,21 @@ class Solution:
 
         for char in s:
             counter[char] = counter.get(char, 0) + 1
-
+        
         for char in t:
             if char not in counter or counter[char] == 0:
-                 return False
+                return False
             counter[char] -=1
 
         return True
+        # counter = {}
+
+        # for char in s:
+        #     counter[char] = counter.get(char, 0) + 1
+
+        # for char in t:
+        #     if char not in counter or counter[char] == 0:
+        #         return False
+        #     counter[char] -=1
+
+        # return True
