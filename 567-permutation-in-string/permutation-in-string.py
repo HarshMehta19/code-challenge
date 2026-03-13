519b7f4cc3a6b1c1975cb6af6d8a42159116dd7d
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2026-03-12 22:18:42 -0300

    Time: 15 ms (82.14%) | Memory: 19.3 MB (96.28%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,37 +1,26 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2): return False
+        n1 = len(s1)
+        n2 = len(s2)
 
-        s1Count, s2Count = [0] * 26, [0] * 26
+        if n1 > n2: return False
 
-        for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord('a')]+=1
-            s2Count[ord(s2[i]) - ord('a')]+=1
+        s1_freq, s2_freq = {}, {}
 
-        matches = 0
-        for i in range(26):
-            matches += (1 if s1Count[i] == s2Count[i] else 0)
+        for i in range(n1):
+            s1_freq[s1[i]] = 1+s1_freq.get(s1[i], 0)
+            s2_freq[s2[i]] = 1+s2_freq.get(s2[i], 0)
 
-        l = 0
-        for r in range(len(s1), len(s2)):
-            if matches == 26: return True
+        if s1_freq == s2_freq:
+            return True
 
-            index = ord(s2[r]) - ord('a')
-            s2Count[index]+=1
-            if s1Count[index] == s2Count[index]:
-                matches +=1
-            elif s1Count[index] + 1 == s2Count[index]:
-                matches -=1
+        for i in range(n1, n2):
+            s2_freq[s2[i]] = 1 + s2_freq.get(s2[i], 0)
+            s2_freq[s2[i - n1]] -=1
+            if not s2_freq[s2[i-n1]]:
+                del s2_freq[s2[i-n1]]
+            
+            if s1_freq == s2_freq:
+                return True
 
-            index = ord(s2[l]) - ord('a')
-            s2Count[index] -=1
-
-            if s1Count[index] == s2Count[index]:
-                matches +=1
-            elif s1Count[index] - 1 == s2Count[index]:
-                matches -=1
-
-            l+=1
-
-        return matches == 26
-        
+        return False
