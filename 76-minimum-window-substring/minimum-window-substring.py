4ff3014f53fa790a2742907ee454dab4c9c95a1d
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2026-03-12 21:52:55 -0300

    Time: 63 ms (75.27%) | Memory: 19.7 MB (91.21%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,36 +1,34 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if s == t: return t
+        #handle case where s and t are same
+        if s == t: return s
 
-        countT, window = {}, {}
+        # 1. create a count of t
+        count_of_t, window = {}, {}
 
         for c in t:
-            countT[c]=1 + countT.get(c, 0)
+            count_of_t[c] = count_of_t.get(c, 0) + 1
         
-        have, need = 0, len(countT)
+        # 2. have =0, need = len(t)
+        have, need = 0, len(count_of_t)
+        # 3. resCorrd [x, y], resLen - to keep the track of min result length
         res, resLen = [-1, -1], float("inf")
         l = 0
+        # 4. Iterate over s, check frequency of character on update have if the frequency of s[i] in window = s[i] in t
         for r in range(len(s)):
             c = s[r]
-            window[c] = 1+window.get(c,0)
-
-            if c in countT and countT[c] == window[c]:
+            window[c] = 1+ window.get(c, 0)
+            # 5. once have == need, compare window and update result [left, right], resLen
+            if c in count_of_t and count_of_t[c] == window[c]:
                 have+=1
-
+        # 6. Narrow down the sequence til have == need by increasing right left side pointer
             while have == need:
-                if (r-l+1) < resLen:
+                if (r - l + 1) < resLen:
+                    resLen = r- l + 1
                     res = [l, r]
-                    resLen = r - l + 1
-                
-                window[s[l]] -=1
-                if s[l] in countT and window[s[l]] < countT[s[l]]:
+
+                window[s[l]]-=1
+                if s[l] in count_of_t and window[s[l]] < count_of_t[s[l]]:
                     have-=1
-
-                l +=1
-
-        l, r = res[0], res[1]
-        return s[l:r+1] if resLen != float("inf") else ""
-
-
-
-
+                l+=1
+        return s[res[0]: res[1] + 1] if resLen != float("inf") else ""
