ab1062550dc7c67f079f005144866eeca965127d
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2026-03-14 02:24:02 -0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (88.41%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,19 +1,15 @@
 class Solution:
-    # Modified binary search: if subarray is sorted, update result and break; otherwise, compare mid with start to decide which half to search while tracking the minimum.
-
     def findMin(self, nums: List[int]) -> int:
-        res = nums[0]
-        start, end = 0, len(nums) - 1
+        left, right = 0, len(nums) - 1
+        # res = nums[0]
 
-        while start <= end:
-            if nums[start] < nums[end]:
-                res = min(res, nums[start])
-                break
-            mid = start + (end - start) // 2
-            res = min(res, nums[mid])
-            if nums[mid] >= nums[start]:
-                start = mid + 1
+        while left < right:
+            
+            mid = (left + right) // 2
+            
+            if nums[mid] <= nums[right]:
+                right = mid
             else:
-                end = mid - 1
-
-        return res
+                left = mid + 1
+        
+        return nums[left]
