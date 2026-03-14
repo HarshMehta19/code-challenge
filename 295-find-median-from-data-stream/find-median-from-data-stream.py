016e036dc71a36efaae88bbbf172db0dbe888194
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2026-03-14 16:06:42 -0300

    Time: 174 ms (64.38%) | Memory: 41.5 MB (91.08%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -14,17 +14,17 @@ class MedianFinder:
             heapq.heappush(self.large, val)
         if len(self.large) > len(self.small) + 1:
             val = heapq.heappop(self.large)
-            heapq.heappush(self.small, -1 * val)
-        
+            heapq.heappush(self.small, -1 * val)        
 
     def findMedian(self) -> float:
+
         if len(self.small) > len(self.large):
-            return -1 * self.small[0]
-        
+            return self.small[0] * -1
         elif len(self.large) > len(self.small):
             return self.large[0]
-
-        return (-1 * self.small[0] + self.large[0]) / 2.0
+        else:
+            return ((-1 * self.small[0]) + self.large[0]) / 2.0
+        
         
 
 
