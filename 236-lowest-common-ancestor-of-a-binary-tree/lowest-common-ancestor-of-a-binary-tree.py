3f4f49445e89a74babae342db280569a24867e5d
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2026-03-14 23:04:55 -0300

    Time: 47 ms (97.78%) | Memory: 24.7 MB (26.17%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,26 +7,39 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        parent = {root: None}
-        queue = deque([root])
+        # ---------------------DFS------------------------
+        if not root or root is p or root is q:
+            return root
 
-        while p not in parent or q not in parent:
-            node = queue.popleft()
-            if node.left:
-                parent[node.left] = node
-                queue.append(node.left)
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
 
-            if node.right:
-                parent[node.right] = node
-                queue.append(node.right)
+        if left and right:
+            return root
 
-        ancestors = set()
+        return left if left else right
 
-        while p:
-            ancestors.add(p)
-            p = parent[p]
+        # ---------------------BFS------------------------
+        # parent = {root: None}
+        # queue = deque([root])
 
-        while q not in ancestors:
-            q = parent[q]
+        # while p not in parent or q not in parent:
+        #     node = queue.popleft()
+        #     if node.left:
+        #         parent[node.left] = node
+        #         queue.append(node.left)
 
-        return q
+        #     if node.right:
+        #         parent[node.right] = node
+        #         queue.append(node.right)
+
+        # ancestors = set()
+
+        # while p:
+        #     ancestors.add(p)
+        #     p = parent[p]
+
+        # while q not in ancestors:
+        #     q = parent[q]
+
+        # return q
