29ca556485680831aeb9699d249d583ba023a65e
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2026-03-19 22:51:02 -0300

    Time: 1 ms (84.19%) | Memory: 20.5 MB (54.17%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,35 +1,62 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        prerequi = {i : [] for i in range(numCourses)}
-        visited = set()
-        visiting = set()
+        
+        # Kanh's algorith
+        indegree = [0] * numCourses
+        adj = [[] for _ in range(numCourses)]
+        for src, dst in prerequisites:
+            indegree[dst] +=1
+            adj[src].append(dst)
+
+        q = deque()
+        for n in range(numCourses):
+            if indegree[n] == 0:
+                q.append(n)
         res = []
+        finish = 0
+        while q:
+            crs = q.popleft()
+            finish +=1
+            res.append(crs)
 
-        for crs, pre in prerequisites:
-            prerequi[crs].append(pre)
+            for nei in adj[crs]:
+                indegree[nei] -=1
+                if indegree[nei] == 0:
+                    q.append(nei)
 
-        def dfs(course):
-            if course in visiting:
-                return False
-            if course in visited:
-                return True
-            
-            visiting.add(course)
-
-            for adj in prerequi[course]:
-                if not dfs(adj):
-                    return False
-
-            visiting.remove(course)
-            visited.add(course)
-            res.append(course)
-            return True
-
-        for crs in range(numCourses):
-            if not dfs(crs):
-                return []
-
-        return res
+        if finish != numCourses:
+            return []
+        
+        return res[::-1]
 
         
+        # prerequi = {i : [] for i in range(numCourses)}
+        # visited = set()
+        # visiting = set()
+        # res = []
+
+        # for crs, pre in prerequisites:
+        #     prerequi[crs].append(pre)
+
+        # def dfs(course):
+        #     if course in visiting:
+        #         return False
+        #     if course in visited:
+        #         return True
             
+        #     visiting.add(course)
+
+        #     for adj in prerequi[course]:
+        #         if not dfs(adj):
+        #             return False
+
+        #     visiting.remove(course)
+        #     visited.add(course)
+        #     res.append(course)
+        #     return True
+
+        # for crs in range(numCourses):
+        #     if not dfs(crs):
+        #         return []
+
+        # return res
