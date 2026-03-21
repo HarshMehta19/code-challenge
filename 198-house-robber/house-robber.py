ecc6b8d36e0d1bff92a77cd9a9653a2d6ff4ca22
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2026-03-20 21:42:16 -0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (80.10%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,15 +1,29 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp = [-1] * len(nums)
-        def rec(i):
-            if i >= len(nums):
-                return 0
-            
-            if i == len(nums) - 1: return nums[i]
+        #--------------------------Bottom Up ------------------------------------#
+        if not nums: return 0
 
-            if dp[i] != -1: return dp[i]
-            
-            dp[i] = max(nums[i] + rec(i+2), rec(i+1))
-            return dp[i]
+        if len(nums) == 1: return nums[0]
 
-        return max(rec(0), rec(1))
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+
+        return dp[-1]
+
+        #--------------------------Top down ------------------------------------#
+        # dp = [-1] * len(nums)
+        # def rec(i):
+        #     if i >= len(nums):
+        #         return 0
+            
+        #     if i == len(nums) - 1: return nums[i]
+
+        #     if dp[i] != -1: return dp[i]
+            
+        #     dp[i] = max(nums[i] + rec(i+2), rec(i+1))
+        #     return dp[i]
+
+        # return max(rec(0), rec(1))
