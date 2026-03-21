e264fad1bd06da9ec83be99f404c85868e72d8db
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2026-03-21 00:25:13 -0300

    Time: 499 ms (71.16%) | Memory: 19.6 MB (88.81%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,20 +1,39 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = {}
-
-        def dfs(amount):
-            if amount == 0:
-                return 0
-            if amount in memo:
-                return memo[amount]
-            
-            res = 1e9
-            for coin in coins:
-                if amount - coin >= 0:
-                    res = min(res, 1 + dfs(amount - coin))
-            
-            memo[amount] = res
-            return res
         
-        minCoins = dfs(amount)
-        return -1 if minCoins >= 1e9 else minCoins
+        #---------------------------- Bottom Up ---------------------------#
+
+        mem = [amount + 1] * (amount + 1)
+        mem[0] = 0
+
+        for a in range(1, amount + 1):
+            for coin in coins:
+                if a - coin >=0:
+                    mem[a] = min(mem[a], 1 + mem[a - coin])
+
+        return mem[amount] if mem[amount] != amount + 1 else -1
+
+
+        #---------------------------- Top Down ---------------------------#
+        # coins.sort()
+        # memo = {}
+        # def rec(target):
+        #     if target == 0:
+        #         return 0
+
+        #     if target in memo:
+        #         return memo[target]
+
+        #     minRes = float("inf")
+
+        #     for i in coins:
+        #         if target - i >=0:
+        #             minRes = min(minRes, 1 + rec(target - i))
+
+        #     memo[target] = minRes
+        #     return memo[target]
+
+        # res = rec(amount)
+        # return res if res != float("inf") else -1
+            
+            
