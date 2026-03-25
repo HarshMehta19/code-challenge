b601917b5b5ba6ba8d7a29ab324a4a2f9a000a65
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2026-03-25 18:55:16 -0300

    Time: 2 ms (22.22%) | Memory: 19.7 MB (99.32%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -6,22 +6,58 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        res = []
-        if not root: return res
 
+        #------------------- BFS---------------------------#
+        res = []
         q = deque()
         q.append(root)
 
         while q:
-            qLen = len(q)
             level = []
-            for _ in range(qLen):
+            for _ in range(len(q)):
                 node = q.popleft()
-                level.append(node.val)
-                if node.left:
+                if node:
+                    level.append(node.val)
                     q.append(node.left)
-                if node.right:
                     q.append(node.right)
-            res.append(level)
-        
+
+            if level:
+                res.append(level)
+
         return res
+
+
+        #-------------------- Recrusion---------------------#
+        # res = []
+        # def dfs(root, level):
+        #     if not root:
+        #         return None
+        #     if len(res) == level:
+        #         res.append([])
+
+        #     res[level].append(root.val)
+        #     dfs(root.left, level + 1)
+        #     dfs(root.right, level + 1)
+
+        # dfs(root, 0)
+        # return res
+
+        # res = []
+        # if not root: return res
+
+        # q = deque()
+        # q.append(root)
+
+        # while q:
+        #     qLen = len(q)
+        #     level = []
+        #     for _ in range(qLen):
+        #         node = q.popleft()
+        #         level.append(node.val)
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+        #     res.append(level)
+        
+        # return res
