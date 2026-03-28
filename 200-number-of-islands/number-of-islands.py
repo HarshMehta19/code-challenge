caf0f77595f5b94845c1ff2c3e2b4eb1df1003e1
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2026-03-28 11:57:21 -0300

    Time: 240 ms (69.32%) | Memory: 21.7 MB (43.25%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,19 +1,43 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        #----------------Iterative----------------
         res = 0
         rows, cols = len(grid), len(grid[0])
-        def dfs(row: int, col: int):
-            if row < 0 or row >= rows or col < 0 or col >= cols or grid[row][col] != '1':
-                return
-            grid[row][col] = '0'
-            dfs(row+1, col)
-            dfs(row-1, col)
-            dfs(row, col + 1)
-            dfs(row, col - 1)
-
+        q = deque()
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == '1':
                     res+=1
-                    dfs(row, col)
+                    q.append((row, col))
+
+                    while q:
+                        x, y = q.popleft()
+                        if x < 0 or x >= rows or y < 0 or y >= cols or grid[x][y] != '1':
+                            continue
+                        grid[x][y] = '3'
+                        q.append((x+1, y))
+                        q.append((x-1, y))
+                        q.append((x, y+1))
+                        q.append((x, y-1))
+
         return res
+
+
+        #----------------Recursive----------------
+        # res = 0
+        # rows, cols = len(grid), len(grid[0])
+        # def dfs(row: int, col: int):
+        #     if row < 0 or row >= rows or col < 0 or col >= cols or grid[row][col] != '1':
+        #         return
+        #     grid[row][col] = '0'
+        #     dfs(row+1, col)
+        #     dfs(row-1, col)
+        #     dfs(row, col + 1)
+        #     dfs(row, col - 1)
+
+        # for row in range(rows):
+        #     for col in range(cols):
+        #         if grid[row][col] == '1':
+        #             res+=1
+        #             dfs(row, col)
+        # return res
