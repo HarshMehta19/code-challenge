c2bdb8d8f5231fa3df086c2953e030f623a24d83
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2026-03-28 23:42:11 -0300

    Time: 25 ms (80.00%) | Memory: 44.5 MB (32.17%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -19,27 +19,42 @@ class Node {
 */
 
 class Solution {
-    public Node cloneGraph(Node node) {
+    private Node clone(Map<Integer, Node> map, Node node) {
         if(node == null) return null;
+        if(map.containsKey(node.val)) return map.get(node.val);
 
         Node newNode = new Node(node.val);
-        Queue<Node> q = new LinkedList<>();
-        HashMap<Node, Node> memory = new HashMap<>();
-        memory.put(node, newNode);
-        q.add(node);
-        while(!q.isEmpty()) {
-            var tempNode = q.poll();
-            if(tempNode!= null) {
-                for(var child : tempNode.neighbors) {
-                    if(!memory.containsKey(child)){
-                        var childNode = new Node(child.val);
-                        memory.put(child, childNode);
-                        q.add(child);
-                    }
-                    memory.get(tempNode).neighbors.add(memory.get(child));
-                }
-            }
+        map.put(node.val, newNode);
+        for(var n: node.neighbors) {
+            newNode.neighbors.add(clone(map, n));
         }
         return newNode;
+    }
+    public Node cloneGraph(Node node) {
+        // ----------------- Recursive --------------
+        Map<Integer, Node> map = new HashMap<>();
+        return clone(map, node);
+        // ------------------Iterative------------
+    //     if(node == null) return null;
+
+    //     Node newNode = new Node(node.val);
+    //     Queue<Node> q = new LinkedList<>();
+    //     HashMap<Node, Node> memory = new HashMap<>();
+    //     memory.put(node, newNode);
+    //     q.add(node);
+    //     while(!q.isEmpty()) {
+    //         var tempNode = q.poll();
+    //         if(tempNode!= null) {
+    //             for(var child : tempNode.neighbors) {
+    //                 if(!memory.containsKey(child)){
+    //                     var childNode = new Node(child.val);
+    //                     memory.put(child, childNode);
+    //                     q.add(child);
+    //                 }
+    //                 memory.get(tempNode).neighbors.add(memory.get(child));
+    //             }
+    //         }
+    //     }
+    //     return newNode;
     }
 }
