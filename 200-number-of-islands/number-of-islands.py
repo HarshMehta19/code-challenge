c56f9cebed9c468e42fddb1c29cb24a9b9ffe4c8
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2026-04-08 23:55:19 -0300

    Time: 241 ms (66.77%) | Memory: 21.5 MB (93.86%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -4,21 +4,23 @@ class Solution:
         res = 0
         rows, cols = len(grid), len(grid[0])
         q = deque()
+
+
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == '1':
                     res+=1
                     q.append((row, col))
 
-                    while q:
-                        x, y = q.popleft()
-                        if x < 0 or x >= rows or y < 0 or y >= cols or grid[x][y] != '1':
-                            continue
-                        grid[x][y] = '3'
-                        q.append((x+1, y))
-                        q.append((x-1, y))
-                        q.append((x, y+1))
-                        q.append((x, y-1))
+                while q:
+                    x, y = q.popleft()
+                    if x < 0 or x >= rows or y < 0 or y >= cols or grid[x][y] != '1':
+                        continue
+                    grid[x][y]= '3'
+                    q.append((x+1, y))
+                    q.append((x-1, y))
+                    q.append((x, y+1))
+                    q.append((x, y-1))
 
         return res
 
