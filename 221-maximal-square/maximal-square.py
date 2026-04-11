6578d7498bc3c801fbf8626ac921c21875b3bcfe
HarshMehta19 <51777373+HarshMehta19@users.noreply.github.com>
2026-04-11 00:27:55 -0300

    Time: 71 ms (89.40%) | Memory: 33.3 MB (33.31%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -1,21 +1,39 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        cache = {}
+
+        # ---------------- Bottom - up -----------------------
         ROWS, COLS = len(matrix), len(matrix[0])
-        def helper(row, col):
-            if row >= ROWS or col >= COLS:
-                return 0
+        dp = [[0] * (COLS + 1) for _ in range(ROWS + 1)]
+        max_square = 0
 
-            if (row, col) not in cache:
-                down = helper(row+1, col)
-                dia = helper(row + 1, col + 1)
-                bottom= helper(row, col + 1)
-
-                cache[(row,col)] = 0
+        for row in range(ROWS - 1, -1, -1):
+            for col in range(COLS - 1, -1, -1):
                 if matrix[row][col] == "1":
-                    cache[(row,col)] = 1 + min(down, dia, bottom)
+                    down = dp[row + 1][col]
+                    dia = dp[row + 1][col + 1]
+                    bottom = dp[row][col + 1]
 
-            return cache[(row, col)]
+                    dp[row][col] = 1 + min(down, dia, bottom)
+                    max_square = max(max_square, dp[row][col])
 
-        helper(0,0)
-        return max(cache.values()) ** 2
+        return max_square**2
+        # ------------------- Top-down------------------------
+        # cache = {}
+        # ROWS, COLS = len(matrix), len(matrix[0])
+        # def helper(row, col):
+        #     if row >= ROWS or col >= COLS:
+        #         return 0
+
+        #     if (row, col) not in cache:
+        #         down = helper(row+1, col)
+        #         dia = helper(row + 1, col + 1)
+        #         bottom= helper(row, col + 1)
+
+        #         cache[(row,col)] = 0
+        #         if matrix[row][col] == "1":
+        #             cache[(row,col)] = 1 + min(down, dia, bottom)
+
+        #     return cache[(row, col)]
+
+        # helper(0,0)
+        # return max(cache.values()) ** 2
